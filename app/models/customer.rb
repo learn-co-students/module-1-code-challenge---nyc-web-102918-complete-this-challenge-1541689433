938b4c912771e04b91a162ae08f27c7b5e8d94b5
github username <github email address>
2018-11-08 11:07:23 -0500

    only managed to do the basics

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,53 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+def self.all
+  @@all
+end
+
+def self.find_by_name(customer)
+  Review.all.find do |review|
+    review.customer == customer
+  end
+end
+
+def Customer.find_all_by_first_name(name)
+  Review.all.select do |review|
+    review.customer.first_name = name
+  end
+  end
+
+  def self.all_names
+    array = []
+    @@all.map do |customer_object|
+      temp =[]
+      temp << customer_object.first_name
+      temp << customer_object.last_name
+      array << temp.join(" ")
+    end
+    array
+  end
+
+
+
+
+
+
+
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,26 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+
+customer1 = Customer.new("eizik", "gottesfeld")
+customer2 = Customer.new("samuel", "taub")
+customer3 = Customer.new("babby", "gottesfeld")
+
+
+restaurant1 = Restaurant.new("restaurant1")
+restaurant2 = Restaurant.new("restaurant2")
+restaurant3 = Restaurant.new("restaurant3")
+
+
+review1 = Review.new(restaurant1, customer1,"great" ,5)
+review2 = Review.new(restaurant2, customer2, "good",4)
+review3 = Review.new(restaurant3, customer3, "bad",3)
+
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
