b01d4477487836a9f96897259b1326942c12eee0
github username <github email address>
2018-11-08 11:07:13 -0500

    We got some work to do

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,22 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+restaurant1 = Restaurant.new("Petes")
+restaurant2 = Restaurant.new("Joes")
+restaurant3 = Restaurant.new("Bettys")
+restaurant4 = Restaurant.new("Janes")
+
+customer1 = Customer.new("Bruce", "Hopewell")
+customer2 = Customer.new("Adam", "Smith")
+customer3 = Customer.new("Paul", "Jones")
+customer4 = Customer.new("Betty", "Davis")
+
+review1 = Review.new(restaurant1, customer1)
+
+
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
