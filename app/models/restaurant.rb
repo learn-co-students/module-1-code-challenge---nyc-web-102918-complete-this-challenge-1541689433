dbc1f0ad58e0c26b362f4808a17cd213a4e03be8
Brian Horowitz <guitarrook94@gmail.com>
2018-11-08 11:07:12 -0500

    I am awesome

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,30 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|mrr| mrr.name == name}
+  end
+
+  def customers
+    Review.all.select {|sn| if sn.restaurant == self.name
+    sn.customer}.uniq
+    #This method should return a unique array of customers who have reviewed this restaurant before. To do this, I should reach into the review class, pick from the all variable, and for every review that matches the restaurant, return the customer value.
+  end
+
+  def reviews
+    Review.all.select {|rest| rest.restaurant == self.name}
+  end
+
+  def average_star_rating
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,34 @@
 class Review
-  
-end
+  @@all = []
+  attr_reader :customer, :restaurant
+  attr_accessor :rating, :content
 
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    self.customer
+  end
+
+  def restaurant
+    self.restaurant
+  end
+
+  def rating
+    self.rating
+  end
+
+  def content
+    self.content
+  end
+
+end
